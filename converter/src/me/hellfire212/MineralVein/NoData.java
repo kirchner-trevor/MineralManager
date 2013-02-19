a6c7d0db7334527a094f0457c145697c44ceabfb
crast <contact@jamescrasta.com>
2013-02-19 02:05:59 -0700

    Got rid of the MM 1.3 converter dependency, cleaned up the Upgrader

--- converter/src/me/hellfire212/MineralVein/NoData.java
+++
@@ -1,9 +0,0 @@
-package me.hellfire212.MineralVein;
-
-public class NoData extends Exception {
-	private static final long serialVersionUID = -4100125232824435169L;
-
-	public NoData(String string) {
-		super(string);
-	}
-}
