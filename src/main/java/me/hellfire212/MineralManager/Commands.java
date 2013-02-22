c637546ea59c52816b5cc4ae6186fb96c637ed38
crast <contact@jamescrasta.com>
2013-02-21 17:06:15 -0700

    Declarative command registration for great win

--- src/main/java/me/hellfire212/MineralManager/Commands.java
+++ src/main/java/me/hellfire212/MineralManager/Commands.java
@@ -16,6 +16,7 @@ import mondocommand.FormatConfig;
 import mondocommand.MondoCommand;
 import mondocommand.MondoFailure;
 import mondocommand.SubHandler;
+import mondocommand.dynamic.Sub;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -42,52 +43,14 @@ public final class Commands {
 
         MondoCommand base = new MondoCommand(config);
         plugin.getCommand("mm").setExecutor(base);
-
-        base.addSub("create")
-            .setDescription("Create a new region")
-            .setHandler(new SubHandler() {
-                public void handle(CallInfo call) {
-                    new CreateRegion(plugin).begin(call.getPlayer(), call.getArgs());
-                }
-                
-            });
+        // This is to set up the ordering, since iterating a class is not predictable.
+        base.addSub("list");
+        base.addSub("create");
+        base.addSub("remove");
+        base.addSub("lock");
         
-        base.addSub("remove")
-            .allowConsole()
-            .setDescription("Remove MM region")
-            .setMinArgs(1)
-            .setUsage("region name")
-            .setHandler(new SubHandler() {
-                public void handle(CallInfo call) {
-                    remove(call);
-                }
-            });
-        
-        base.addSub("list")
-            .allowConsole()
-            .setDescription("List MM regions")
-            .setHandler(new SubHandler() {
-                public void handle(CallInfo call) throws MondoFailure {
-                    list(call);
-
-                }            
-            });
-        
-        base.addSub("lock")
-            .setDescription("Lock block on your cursor")
-            .setHandler(new SubHandler() {
-                public void handle(CallInfo call) throws MondoFailure {
-                    lock(call);
-                }
-            });
-        
-        base.addSub("creative")
-            .setDescription("Creative Mode")
-            .setHandler(new SubHandler() {
-                public void handle(CallInfo call) throws MondoFailure {
-                    creative(call, call.getPlayer());
-                }            
-            });
+        // Add all other subs automagically
+        base.autoRegisterFrom(this);
         
         plugin.getCommand("test").setExecutor(new CommandExecutor() {
             public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -107,104 +70,13 @@ public final class Commands {
         ChatMagic.registerAlias("{TEXT}", ChatColor.LIGHT_PURPLE);
         ChatMagic.registerAlias("{VERB}", ChatColor.GREEN);
 	}
-	//These methods all make the assumption that "args" contains the correct amount of arguments of the correct type.
 	
-	// TODO refactor this to a utility module or something
-	public static Region actuallyCreateRegion(MineralManager plugin, String name, Configuration configuration, Selection selection, Player player, int level) {
-		Region newRegion = new Region(name, configuration, selection.getShape(), selection.getFloor(), selection.getCeil(), player.getWorld(), level);
-		WorldData wdata = plugin.getWorldData(player.getWorld());
-		boolean regionAdded = wdata.getRegionSet().add(newRegion);
-		wdata.flagRegionSetDirty();
-		return regionAdded? newRegion: null;
+	@Sub(description="Create a new region", allowConsole=false)
+	public void create(CallInfo call) {
+        new CreateRegion(plugin).begin(call.getPlayer(), call.getArgs());
 	}
 
-
-	public static Selection selectWorld(MineralManager plugin, Player player) {
-		player.sendMessage(MineralManager.PREFIX + "Selected whole world " + player.getWorld().getName());
-		return new Selection(null, -1.0D, -1.0D);
-	}
-
-	public static void beginLasso(Player player) {
-		//Tells the lassoListener that there is one more person listening for lasso selections.
-		if(!lassoCoordinateMap.containsKey(player.getName())) {
-			MineralManager.getInstance().lassoListener.add();
-		}
-		
-		//Adds the player to the lassoCoordinateMap so points can be added.
-		lassoCoordinateMap.put(player.getName(), new ArrayList<Coordinate>());
-		new LassoWatcherTask(player.getName()).run();
-	}
-	
-	public static Selection finishLasso(Player player, String prefix) {
-		ArrayList<Point2D> boundaries = new ArrayList<Point2D>();
-		ArrayList<Coordinate> temp = lassoCoordinateMap.get(player.getName());
-		double floor = Integer.MAX_VALUE;
-		double ceil = Integer.MIN_VALUE;
-		for(Coordinate coordinate : temp) {
-			double y = coordinate.getY();
-			if (y < floor) {
-				floor = y;
-			}
-			if (y > ceil) {
-				ceil = y;
-			}
-			boundaries.add(new Point2D.Double(coordinate.getX(), coordinate.getZ()));
-		}
-		boundaries = ShapeUtils.reduceBoundaries(boundaries);
-		
-		Polygon poly = new Polygon();
-		for (Point2D p: boundaries) {
-			ShapeUtils.addPolyPoint(poly, p);
-		}
-		
-		// Tells the lassoListener that there is one less person listening for lasso selections.
-		MineralManager.getInstance().lassoListener.remove();
-		
-		//Removes the player from the lassoCoordinateMap since we completed our selection.
-		Commands.lassoCoordinateMap.remove(player.getName());
-		
-		player.sendMessage(prefix + "Finished recording.");
-		return new Selection(poly, floor, ceil);
-	}
-
-	public static Selection selectCube(MineralManager plugin, Player player, int xzRadius, int yRadius) {
-
-		int playerX = player.getLocation().getBlockX();
-		int playerY = player.getLocation().getBlockY();
-		int playerZ = player.getLocation().getBlockZ();
-		int west = playerX - xzRadius;
-		int south = playerZ - xzRadius;
-		int east = playerX + xzRadius;
-		int north = playerZ + xzRadius;
-		
-		Rectangle rect = new Rectangle(west, south, xzRadius *2, xzRadius * 2);
-
-		player.sendMessage(MineralManager.PREFIX + "A cube spanning (" +  west + ", " + south + ") to (" + east + ", " + north + ") was selected.");
-		return new Selection(rect, playerY - yRadius, playerY + yRadius);
-	}
-	
-	/** Not a command, but functionality used by the dialogue to do the actual selection. */
-	public static Selection actuallySelectRegion(MineralManager plugin, Player player, Location startCoordinate, Location endCoordinate, String prefix) {
-		int x1 = startCoordinate.getBlockX();
-		int z1 = startCoordinate.getBlockZ();
-		int x2 = endCoordinate.getBlockX();
-		int z2 = endCoordinate.getBlockZ();
-		
-		double y1 = startCoordinate.getY();
-		double y2 = endCoordinate.getY();
-		
-		Rectangle rect = new Rectangle(
-				Math.min(x1, x2),
-				Math.min(z1, z2),
-				Math.abs(x1 - x2),
-				Math.abs(z1 - z2)
-		);
-		
-		player.sendMessage(prefix + "A cube spanning (" + x1 + ", " +  z1 + ") to (" + x2 + ", " + z2 + ") was selected.");
-		return new Selection(rect, Math.min(y1, y2), Math.max(y1, y2));
-	}
-
-	//1 Argument
+	@Sub(description="Remove MM region", minArgs=1, usage="region name")
 	public void remove(CallInfo call) {
 		String status = "was not";
 		String regionName = call.getArg(0);
@@ -217,7 +89,7 @@ public final class Commands {
 		call.reply("%s %s removed", regionName, status);
 	}
 	
-	//0 Arguments
+	@Sub(description = "List MM Regions")
 	public void list(CallInfo call) {
 		call.reply("{HEADER}[Region List]");
 		Collection<WorldData> wds = plugin.allWorldDatas();
@@ -230,7 +102,7 @@ public final class Commands {
 		}
 	}
 
-	//0 Arguments
+	@Sub(description = "Lock block on your cursor")
 	public void lock(CallInfo call) {
 		Block targetBlock = call.getPlayer().getTargetBlock(null, 20); //The 20 is the maximum distance away a block can be to be "selected".
 		if(targetBlock != null) {
@@ -249,8 +121,9 @@ public final class Commands {
 		}
 	}
 	
-	//0 Arguments
-	public void creative(CallInfo call, Player player) {
+	@Sub(description = "Creative Mode", allowConsole = false)
+	public void creative(CallInfo call) {
+	    Player player = call.getPlayer();
 		String status = "NORMAL";
 		if(player.hasMetadata(MineralListener.METADATA_CREATIVE)) {
 			player.removeMetadata(MineralListener.METADATA_CREATIVE, plugin);
@@ -266,4 +139,99 @@ public final class Commands {
 		lassoCoordinateMap.clear();
 	}
 
+    
+    // TODO refactor this to a utility module or something
+    public static Region actuallyCreateRegion(MineralManager plugin, String name, Configuration configuration, Selection selection, Player player, int level) {
+        Region newRegion = new Region(name, configuration, selection.getShape(), selection.getFloor(), selection.getCeil(), player.getWorld(), level);
+        WorldData wdata = plugin.getWorldData(player.getWorld());
+        boolean regionAdded = wdata.getRegionSet().add(newRegion);
+        wdata.flagRegionSetDirty();
+        return regionAdded? newRegion: null;
+    }
+
+
+    public static Selection selectWorld(MineralManager plugin, Player player) {
+        player.sendMessage(MineralManager.PREFIX + "Selected whole world " + player.getWorld().getName());
+        return new Selection(null, -1.0D, -1.0D);
+    }
+
+    public static void beginLasso(Player player) {
+        //Tells the lassoListener that there is one more person listening for lasso selections.
+        if(!lassoCoordinateMap.containsKey(player.getName())) {
+            MineralManager.getInstance().lassoListener.add();
+        }
+        
+        //Adds the player to the lassoCoordinateMap so points can be added.
+        lassoCoordinateMap.put(player.getName(), new ArrayList<Coordinate>());
+        new LassoWatcherTask(player.getName()).run();
+    }
+    
+    public static Selection finishLasso(Player player, String prefix) {
+        ArrayList<Point2D> boundaries = new ArrayList<Point2D>();
+        ArrayList<Coordinate> temp = lassoCoordinateMap.get(player.getName());
+        double floor = Integer.MAX_VALUE;
+        double ceil = Integer.MIN_VALUE;
+        for(Coordinate coordinate : temp) {
+            double y = coordinate.getY();
+            if (y < floor) {
+                floor = y;
+            }
+            if (y > ceil) {
+                ceil = y;
+            }
+            boundaries.add(new Point2D.Double(coordinate.getX(), coordinate.getZ()));
+        }
+        boundaries = ShapeUtils.reduceBoundaries(boundaries);
+        
+        Polygon poly = new Polygon();
+        for (Point2D p: boundaries) {
+            ShapeUtils.addPolyPoint(poly, p);
+        }
+        
+        // Tells the lassoListener that there is one less person listening for lasso selections.
+        MineralManager.getInstance().lassoListener.remove();
+        
+        //Removes the player from the lassoCoordinateMap since we completed our selection.
+        Commands.lassoCoordinateMap.remove(player.getName());
+        
+        player.sendMessage(prefix + "Finished recording.");
+        return new Selection(poly, floor, ceil);
+    }
+
+    public static Selection selectCube(MineralManager plugin, Player player, int xzRadius, int yRadius) {
+
+        int playerX = player.getLocation().getBlockX();
+        int playerY = player.getLocation().getBlockY();
+        int playerZ = player.getLocation().getBlockZ();
+        int west = playerX - xzRadius;
+        int south = playerZ - xzRadius;
+        int east = playerX + xzRadius;
+        int north = playerZ + xzRadius;
+        
+        Rectangle rect = new Rectangle(west, south, xzRadius *2, xzRadius * 2);
+
+        player.sendMessage(MineralManager.PREFIX + "A cube spanning (" +  west + ", " + south + ") to (" + east + ", " + north + ") was selected.");
+        return new Selection(rect, playerY - yRadius, playerY + yRadius);
+    }
+    
+    /** Not a command, but functionality used by the dialogue to do the actual selection. */
+    public static Selection actuallySelectRegion(MineralManager plugin, Player player, Location startCoordinate, Location endCoordinate, String prefix) {
+        int x1 = startCoordinate.getBlockX();
+        int z1 = startCoordinate.getBlockZ();
+        int x2 = endCoordinate.getBlockX();
+        int z2 = endCoordinate.getBlockZ();
+        
+        double y1 = startCoordinate.getY();
+        double y2 = endCoordinate.getY();
+        
+        Rectangle rect = new Rectangle(
+                Math.min(x1, x2),
+                Math.min(z1, z2),
+                Math.abs(x1 - x2),
+                Math.abs(z1 - z2)
+        );
+        
+        player.sendMessage(prefix + "A cube spanning (" + x1 + ", " +  z1 + ") to (" + x2 + ", " + z2 + ") was selected.");
+        return new Selection(rect, Math.min(y1, y2), Math.max(y1, y2));
+    }
 }
